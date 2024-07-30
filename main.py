1c0556ae26c185caaa01c833fa66b42eb8af04b8
kzs-666 <137208020+kzs-666@users.noreply.github.com>
2024-07-30 16:45:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import chime
 
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+score = 0
+highscore = 0
 letter = ""
 answer = ""
 
-print("*** Press Ctrl + C to exit. ***\n")
+print("*** Press Ctrl + C or type 'exit' to exit. ***\n")
 Beep(900, 900)
 time.sleep(3)
 print("Start\n\n")
@@ -22,8 +24,17 @@ while (True):
     answer = input("Letter: ").strip().upper()
     if answer == letter:
         print("Correct!\n\n")
+        score += 1
         chime.success()
         time.sleep(1.2)
+    elif answer == "EXIT":
+        n = int(open("best.txt", "w"))
+        if score > n:
+            n.write(score)
+        n.close()
+        print(f"Your score is {score}. The high score is {highscore}.")
+        time.sleep(5)
+        exit()
     else:
         print(f"Wrong, it was {letter}.\n\n")
         chime.warning()
